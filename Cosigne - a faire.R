3d392a6f165a31817185caff838edb4766c341e2
bleuclem <116887364+bleuclem@users.noreply.github.com>
2022-11-23 11:22:02 +0100

    Add files via upload

---
+++ Cosigne - a faire.R
@@ -0,0 +1,28 @@
+#Consigne quanti : 
+#  Répondre à une question sociologique en utilisant les outil d’R. 
+#Le forme doit mettre en valeur le fond.
+
+#Consigne parallèle :
+#  -	Explicité les méthodes et cheminement dans le rendu 
+# -	Centrer le travail autour d’une question 
+# -	Montrer que l’on a utiliser les outils proposer et que l’on apprit 
+# -	Décortiquer la base de données le plus possible et essayer d’en sortir le plus possible. 
+# -	Parler de nos idées et de ce qu’on n’a pas réussi. 
+
+#Cosigne sur le forme et ce qui est demander dans le rendu : 
+#  -	La forme est libre, elle peut prendre toute forme mais elle doit mettre en valeurs le fond, qui est le plus important. 
+# -	Ecriture de la forme se fait dans R, avec une « file » mark-down (ficher site web) et notebook (ficher texte)
+# -	Plusieurs codage dans le rendu : le codage du rendu lui-même, codages d’exploration de la base de donnée, codage de la configuration du PC qui faire tourner le codage et tout les codages ‘brouillon’. 
+
+#Bases de données : la base est bien. Attention aux questions d’anonymisation 
+
+# Lundi 14 : 
+#  Bases de données : individus de 25 à 35 ans. 
+#1er idée de question : Construction de la vision du célibat. 
+# -	Histoire social 
+# -	Situation socio actuel 
+# -	Parcours amoureux 
+
+#But : voir pour les formulation de problématique 
+#A faire : Trouver d’autres variables, fin de recodage des variables présente
+#Pourquoi pas : trouver des référence sociologique sur le sujet, début d’exploration de la base de données
